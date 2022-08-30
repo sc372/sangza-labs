51984e477c3324eaa39a1260ff344f906e75d70a
372lsc@gmail.com <tosil007>
2022-08-30 23:28:34 +0900

    init menu drawer

--- components/layouts/MainLayout/index.tsx
+++ components/layouts/MainLayout/index.tsx
@@ -2,13 +2,18 @@ import MenuDrawer from "@components/organisms/MenuDrawer";
 import Header from "@components/organisms/Header";
 import { IWrapperComponentProps } from "common/interfaces";
 import { FC } from "react";
+import Backdrop from "@components/atoms/Backdrop";
+import { useIsOpenMenuDrawer } from "@modules/drawer/action";
 
 const MainLayout: FC<IWrapperComponentProps> = ({ children }) => {
+  const { isOpenMenuDrawer } = useIsOpenMenuDrawer();
+
   return (
     <>
       <main className="py-5 px-14">
         <Header />
         <MenuDrawer />
+        {isOpenMenuDrawer && <Backdrop />}
         {children}
       </main>
     </>
