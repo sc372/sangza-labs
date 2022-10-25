69a42ec4b09f550be2dd941b6a450579a60815fc
372lsc@gmail.com <tosil007>
2022-10-25 23:43:05 +0900

    Menu Drawer 내용 추가

--- shared/components/organisms/MenuDrawer/index.tsx
+++ shared/components/organisms/MenuDrawer/index.tsx
@@ -1,11 +1,27 @@
 import { useIsOpenMenuDrawerAction } from '@modules/drawer/action'
+import Image from 'next/image'
 import styles from './styles.module.css'
 
 const MenuDrawer = () => {
   const { isOpenMenuDrawer } = useIsOpenMenuDrawerAction()
   return (
     <nav className={`${styles.menuDrawer} ${isOpenMenuDrawer && styles.open}`}>
-      <section>sldkfjl</section>
+      <h2 className={`${styles.profileTitle}`}>sangza</h2>
+      <div className={`${styles.profileCardContainer}`}>
+        <div className={`${styles.profileCardImage}`}>
+          <Image
+            src="/static/images/profile.jpg"
+            alt="profile"
+            width={500}
+            height={600}
+          />
+          <div className={`${styles.profileWrapper}`}>
+            <h3 className={`${styles.profileTitle}`}>sangza</h3>
+            <p className={`${styles.profileDescription}`}>web engineer</p>
+          </div>
+          <div className={`${styles.profileWrapper}`}></div>
+        </div>
+      </div>
     </nav>
   )
 }
