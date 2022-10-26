7a5a523810a78c112ed67e935cc5b63684a797b5
372lsc@gmail.com <tosil007>
2022-10-26 22:43:55 +0900

    styling menu drawer

--- shared/components/organisms/MenuDrawer/index.tsx
+++ shared/components/organisms/MenuDrawer/index.tsx
@@ -1,33 +1,49 @@
 import { useIsOpenMenuDrawerAction } from '@modules/drawer/action'
 import Image from 'next/image'
+import Link from 'next/link'
+import { RiGithubFill, RiMailLine } from 'react-icons/ri'
 import styles from './styles.module.css'
 
 const MenuDrawer = () => {
-  const { isOpenMenuDrawer } = useIsOpenMenuDrawerAction()
+  const { isOpenMenuDrawer, closeIsOpenMenuDrawer } =
+    useIsOpenMenuDrawerAction()
   return (
     <nav
       className={`${styles['menu-drawer']} ${
         isOpenMenuDrawer && styles['open']
       }`}
     >
-      <h2 className={`${styles['menu-drawer__title']}`}>sangza</h2>
+      <h2 className={`${styles['menu-drawer__title']}`}>think</h2>
       <div className={`${styles['menu-drawer__card']}`}>
-        <div className={`${styles['menu-drawer__card--image']}`}>
-          <Image
-            src="/static/images/profile.jpg"
-            alt="profile"
-            width={500}
-            height={600}
-          />
-          <div className={`${styles['menu-drawer__card--meta']}`}>
-            <h3 className={`${styles['menu-drawer__card--meta-name']}`}>
-              sangza
-            </h3>
-            <p className={`${styles['menu-drawer__card--meta-description']}`}>
-              web engineer
-            </p>
+        <Image
+          src="/static/images/profile.jpg"
+          alt="profile"
+          width={450}
+          height={500}
+        />
+        <div className={`${styles['menu-drawer__card--meta']}`}>
+          <h3 className={`${styles['menu-drawer__card--meta-name']}`}>
+            sangza
+          </h3>
+          <p className={`${styles['menu-drawer__card--meta-description']}`}>
+            web engineer
+          </p>
+        </div>
+        <div className={`${styles['menu-drawer__card--contact']}`}>
+          <div>
+            <RiGithubFill size={30} />
           </div>
-          {/* <div className={`${styles['profile-wrapper']}`}></div> */}
+          <div>
+            <RiMailLine size={30} />
+          </div>
+        </div>
+        <div className={`${styles['menu-drawer__list']}`}>
+          <div onClick={closeIsOpenMenuDrawer}>
+            <Link href={'/'}>home</Link>
+          </div>
+          <div onClick={closeIsOpenMenuDrawer}>
+            <Link href={'/project'}>project</Link>
+          </div>
         </div>
       </div>
     </nav>
--- shared/modules/drawer/action.ts
+++ shared/modules/drawer/action.ts
@@ -6,9 +6,11 @@ export const useIsOpenMenuDrawerAction = () => {
     useRecoilState(isOpenMenuDrawerAtom)
 
   const toggleIsOpenMenuDrawer = () => setIsOpenMenuDrawer(!isOpenMenuDrawer)
+  const closeIsOpenMenuDrawer = () => setIsOpenMenuDrawer(false)
 
   return {
     isOpenMenuDrawer,
     toggleIsOpenMenuDrawer,
+    closeIsOpenMenuDrawer,
   }
 }
