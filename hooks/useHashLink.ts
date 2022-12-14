45968b3c1788470ba7f99c78e4491738229c4b78
372lsc@gmail.com <tosil007>
2022-12-14 18:26:33 +0900

    list handling refactoring

--- hooks/useHashLink.ts
+++ hooks/useHashLink.ts
@@ -1,11 +1,16 @@
 import { RefObject, useEffect, useRef, useState } from 'react'
 
+import * as fpArray from 'fp-ts/Array'
+import * as fpFunction from 'fp-ts/function'
 
-const headElementList = ["H1", "H2", "H3", "H4", "H5"] as const
-type HeadElement = typeof headElementList[number]
 
-const isHeadElement = (a: string) => headElementList.find(aa => a === aa);
+const headingElementList = ["H1", "H2", "H3", "H4", "H5"] as const
+type HeadingElement = typeof headingElementList[number]
+
+const isHeadingElement = (a: HeadingElement) => headingElementList.some(aa => a === aa);
 const getPrefix = (a: string) => [...Array(parseInt(a) * 3)].reduce((aa, bb) => aa + "\u00A0", "")
+const filteredHeadingElement = (a: Element) => isHeadingElement(a.tagName as HeadingElement)
+const getHashLinkElement = (a: Element) => [getPrefix(a.tagName.split("")[1]), a.tagName, a.innerHTML]
 
 export type HashLinkElement = [
   prefix: string,
@@ -23,9 +28,11 @@ export const useHashLink = (): UseHashLink => {
   const [hashLinkElementList, setHashLinkElementList] = useState<Array<HashLinkElement>>([])
 
   function makeHashLinkElementList(ref: HTMLDivElement): Array<HashLinkElement> {
-    return Array.from(ref.children)
-      .filter(a => isHeadElement(a.tagName as HeadElement))
-      .map(a => [getPrefix(a.tagName.split("")[1]), a.tagName, a.innerHTML])
+    return fpFunction.pipe(
+      Array.from(ref.children),
+      fpArray.filter(filteredHeadingElement),
+      fpArray.map(getHashLinkElement)
+    ) as Array<HashLinkElement>
   }
 
   useEffect(() => {
